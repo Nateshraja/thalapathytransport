d879c4c628595f71b769be241fe1a409ad7db547
Natesh.R <Natesh.R@ramraj.local>
2025-06-24 16:32:51 +0530

    home setup

--- transport/urls.py
+++ transport/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
+
+def redirect_to_admin(request):
+    return redirect('/admin/login/')
 
 urlpatterns = [
+    path('', redirect_to_admin, name='redirect_to_admin'),
     path('trips/', views.trip_list, name='trip_list'),
     path('trips/add/', views.trip_entry_view, name='trip_entry'),
     path('trips/<int:pk>/pdf/', views.trip_pdf, name='trip_pdf'),
